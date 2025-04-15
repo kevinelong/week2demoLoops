f2f2c5696797e94431a0ae48593a15e43bb4148c
Kevin Ernest Long <kevinelong@gmail.com>
2025-04-15 10:10:46 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
+        // WHILE LOOP
         int count = 10; //INITIALIZE
-
-        while(count >= 1) { //EVALUATING THE EXPRESSION
+        while (count >= 1) { //EVALUATING THE EXPRESSION
             System.out.println(count);
             count--; // decrement (decrease) count = count - 1
         }
+        System.out.println("BLAST OFF!!! - while");
 
-        System.out.println("BLAST OFF!!!");
+        // DO WHILE LOOP
+        int count2 = 10; //INITIALIZE
+        do { //EVALUATING THE EXPRESSION
+            //BODY ALWAYS HAPPENS AT LEAST ONE IN A DO WHILE
+            System.out.println(count2);
+            count2--; // decrement (decrease) count = count - 1
+        } while (count2 >= 1);
+        System.out.println("BLAST OFF!!! - do/while");
+
+        // CLASSIC FOR LOOP - Counting down
+        for (int i = 10; i >= 1; i--) { //NOTE THE DECREMENT OPERATOR and initialization to 10
+            System.out.println(i);
+        }
+        System.out.println("BLAST OFF!!! - classic for loop with decrement");
+
     }
 }
